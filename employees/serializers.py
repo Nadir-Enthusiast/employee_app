eeaf5b7f7344e5bc840db1bbffa299de0f3377d8
Nadir <nadirkarimov76@gmail.com>
2022-08-05 13:55:41 -0400

    Initialize app

---
+++ employees/serializers.py
@@ -0,0 +1,13 @@
+from dataclasses import fields
+from rest_framework import serializers
+from employees.models import Departments,Employees
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Departments
+        fields=("DepartmentId", "DepartmentName")
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Employees
+        fields=("EmployeeId","EmployeeName","Department","DateOfJoining","PhotoFileName")
