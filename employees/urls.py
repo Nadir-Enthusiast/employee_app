18a32e1e4d8cfc0f319b9cb5f9d2a291a1af3a42
Nadir <nadirkarimov76@gmail.com>
2022-08-06 14:18:46 -0400

    Add urls

---
+++ employees/urls.py
@@ -0,0 +1,16 @@
+import imp
+from django.urls import re_path
+from employees import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    re_path(r'^department$',views.departmentApi),
+    re_path(r'^department/([0-9]+)$',views.departmentApi),
+
+    re_path(r'^employee$',views.employeeApi),
+    re_path(r'^employee/([0-9]+)$',views.employeeApi),
+
+    re_path(r'^employee/savefile',views.SaveFile)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
