3e0e5b96c4a100d9f194f5ac7f613f094b664207
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-09-20 22:32:51 -0400

    Create starter.sql

---
+++ starter.sql
@@ -0,0 +1,6 @@
+select * from public."EmployeeApp_departments";
+insert into public."EmployeeApp_departments" values (1, 'IT');
+insert into public."EmployeeApp_departments" values (2, 'Support');
+
+select * from public."EmployeeApp_employees";
+insert into public."EmployeeApp_employees" values (1, 'Bob', 'IT', '2012-12-12', 'anonymous.png');
